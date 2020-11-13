a09d144f6b626d7e5f28485c0b2f4a1b7086957a
丁坤 <dingkun@dingkundeMBP.attlocal.net>
2020-11-13 13:52:29 -0500

    frame

---
+++ SVD.py
@@ -0,0 +1,8 @@
+def _SVD():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ k_mean.py
@@ -0,0 +1,8 @@
+def _k_mean():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
